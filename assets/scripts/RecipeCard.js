3cd85004520d5af25b4efcb6563873b5391696d8
YGnina <93010504+YGnina@users.noreply.github.com>
2022-11-03 16:55:30 -0700

    typo

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -123,20 +123,22 @@ class RecipeCard extends HTMLElement {
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     
     this.shadowRoot.querySelector("article").innerHTML = `
-    <img src='${data.imgSrc}' alt='${data.imgAlt}'>
+    <img src="${data.imgSrc}"
+    alt="${data.imgAlt}">
     <p class="title">
       <a href="${data.titleLnk}">${data.titleTxt}</a>
     </p>
-    <p class="organization">The Chef's Organization</p>
+    <p class="organization">${data.organization}</p>
     <div class="rating">
       <span>${data.rating}</span>
-      <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+      <img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
       <span>(${data.numRatings})</span>
     </div>
     <time>${data.lengthTime}</time>
     <p class="ingredients">
       ${data.ingredients}
-    </p>`;
+    </p>
+    `;
     
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
