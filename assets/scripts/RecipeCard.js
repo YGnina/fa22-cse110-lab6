a3de21e319f4cc3172c66922a42417646e2f56aa
YGnina <93010504+YGnina@users.noreply.github.com>
2022-11-03 17:16:16 -0700

    debug-link problem

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -139,7 +139,7 @@ class RecipeCard extends HTMLElement {
       ${data.ingredients}
     </p>
     `;
-    
+    //console.log(data.imgSrc);
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
